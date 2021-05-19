0c185025b6c0951ae89223a6383b95045a7e5e59
DoDuongDuy <duyhoten080502@gmail.com>
2021-05-19 22:43:46 +0700

    update

--- music/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'vuex'
-import doc from "./modules/doc.js"
-import auth from "./modules/auth.js"
-export default createStore({
-  modules: {
-    auth,
-    doc
-  }
-})
--- shopping-cart/src/views/Cart.vue
+++ shopping-cart/src/views/Cart.vue
@@ -1,13 +1,28 @@
+ 
 <template>
-  this is card
+    <div>
+        <h1>Your cart</h1>
+        
+        <CartItemCard
+            v-for="item in items"
+            :key="item.id"
+            :item="item" />
+
+        <CartSummaryPaymentCard />
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+    import CartItemCard from '../components/cart/CartItemCard.vue'
+    import CartSummaryPaymentCard from '../components/cart/CartSummaryPaymentCard.vue'
+    export default {
+        components: {
+            CartItemCard, CartSummaryPaymentCard
+        },
+        computed: {
+            items() {
+                return this.$store.getters.cartItems
+            }
+        }
+    }
 </script>
-
-<style>
-
-</style>
